ff4efe49788966df25f86b3dce63a073b931174f
CarlosHeKj <carloslouise2015@gmail.com>
2024-08-13 10:31:42 -0300

    Adding comments to posts

--- src/main/java/com/carloshenrique/workshopmongo/config/Instantiation.java
+++ src/main/java/com/carloshenrique/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.carloshenrique.workshopmongo.domain.Post;
 import com.carloshenrique.workshopmongo.domain.User;
 import com.carloshenrique.workshopmongo.dto.AuthorDTO;
+import com.carloshenrique.workshopmongo.dto.CommentDTO;
 import com.carloshenrique.workshopmongo.repository.PostRepository;
 import com.carloshenrique.workshopmongo.repository.UserRepository;
 
@@ -45,9 +46,17 @@ public class Instantiation implements CommandLineRunner{
 		
 		
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
 		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		
+		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
 		
